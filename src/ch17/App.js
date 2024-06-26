6ba57fd3fcd02294cb98663c9cbdea7d7e765225
ehddls98 <icbku1357@naver.com>
2024-06-26 09:57:34 +0900

    24/06/26 React

---
+++ src/ch17/App.js
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+
+import * as s from "./style";
+
+function App() {
+    return (
+        <div css={s.container}>
+            <div css={s.container2}>
+                <div css={s.container3}>
+                    <div css={s.container4}></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/ch17/style.js
@@ -0,0 +1,38 @@
+import { css } from "@emotion/react";
+
+export const container = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    border: 1px solid black;
+    width: 300px;
+    height: 300px;
+`;
+
+export const container2 = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    border: 1px solid black;
+    width: 70%;
+    height: 70%;
+`;
+
+export const container3 = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    border: 1px solid black;
+    width: 70%;
+    height: 70%;
+`;
+
+export const container4 = css`
+    box-sizing: border-box;
+    border: 1px solid black;
+    width: 70%;
+    height: 70%;
+`;
